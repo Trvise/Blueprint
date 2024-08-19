116f07cd8eee095642a61872c9b12be451459d9c
Aadityavoru <av34@illinois.edu>
2024-08-19 17:48:40 +0530

    no protection

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
 import React, { useState } from "react";
-import Login from "./components/AuthComponents/Login";
-import Register from "./components/AuthComponents/Register";
 import Sidebar from "./components/Layout/Sidebar";
 import UploadVideos from "./components/pages/UploadVideos";
 import VideosList from "./components/pages/VideosList";
-import DevicesList from "./components/pages/DevicesList"; // Import the DevicesList component
-import { AuthProvider} from "./contexts/authContext";
+import DevicesList from "./components/pages/DevicesList";
 import { useRoutes } from "react-router-dom";
-import {ProtectedRoute} from "./components/Layout/ProtectedRoute";
 
 function App() {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const toggleSidebar = () => setIsCollapsed(!isCollapsed);
 
   const routesArray = [
-    { path: "*", element: <Login /> },
-    { path: "/login", element: <Login /> },
-    // { path: "/register", element: <Register /> },
-    { path: "/home", element: (<ProtectedRoute><UploadVideos /></ProtectedRoute>)},
-    { path: "/videos", element: (<ProtectedRoute><VideosList /></ProtectedRoute>) },
-    {path: "/devices", element: (<ProtectedRoute><DevicesList/></ProtectedRoute>)}
+    { path: "/home", element: <UploadVideos /> },
+    { path: "/videos", element: <VideosList /> },
+    { path: "/devices", element: <DevicesList /> },
+    // Redirect any unknown path to /home
+    { path: "*", element: <UploadVideos /> },
   ];
 
   const routesElement = useRoutes(routesArray);
 
   return (
-    <AuthProvider>
-      <div className="flex">
-        <Sidebar isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} />
-        <div className={`transition-all duration-300 ${isCollapsed ? 'ml-16' : 'ml-64'} flex-1 p-8`}>
-          <div className="w-full h-full flex flex-col">{routesElement}</div>
-        </div>
+    <div className="flex">
+      <Sidebar isCollapsed={isCollapsed} toggleSidebar={toggleSidebar} />
+      <div className={`transition-all duration-300 ${isCollapsed ? 'ml-16' : 'ml-64'} flex-1 p-8`}>
+        <div className="w-full h-full flex flex-col">{routesElement}</div>
       </div>
-    </AuthProvider>
+    </div>
   );
 }
 
